658603383ebc89cb18a193c0a625ad6667cade39
shabz5 <47355878+shabz5@users.noreply.github.com>
2019-04-08 19:35:32 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+function greeting () {
+  var name = "unless you are 18 and over or rich";
+  alert(" Warning do not play poker "  +  name);
+
+}
+
+greeting();
+
+
+function newGreeting (name) {
+  var name = "unless you are 18 and over or rich";
+  alert(" Warning do not play poker "  +  name);
+
+}
